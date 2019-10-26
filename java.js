6cd8cbc38db6787bde790188dcd1bfb05df4ca21
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-10-26 14:02:17 -0700

    changed java.js to include a store full of questions and have function stubs

--- java.js
+++ java.js
@@ -1,70 +1,144 @@
 'use strict';
 
 const STORE ={
-  quiz: [],
+  quiz: [
+    {
+      question: 'what are your odds of getting this question right?',
+      options: ['25%', '25%', '50%', '33%'],
+      correct: 3 //array index
+    },
+    {
+      question: 'When was the first multiple choice quiz for computers',
+      options: [1982, 1999, 1973, 'today'],
+      correct: 0 //array index
+    },
+    {
+      question: 'Who gives the best quizzes',
+      options: ['A Sphinx', 'The Riddler', 'This Quiz', 'The Local Bridge Troll'],
+      correct: 2 //array index
+    },
+    {
+      question: 'What was the answer to Question 1?',
+      options: ['A', 'B', 'C', 'D'],
+      correct: 3 //array index
+    },
+    {
+      question: 'What Question Was on This Quiz?',
+      options: ['How often will you get this right?', 'Who gives the best quizzes', 'When was the first multiple choice quiz'
+        'What was the answer to Question 2?'],
+      correct: 1 //array index
+    }
+],
   score: 0,
   currentQuestion: -1
 };
 
 //see first page, returns html for the first page
-function firstPage(){
-    $('main').html('<p>something</p>');
+function startQuiz(){
+  console.log('starting quiz');
+  //increase quiz question number by one
+  //get main page
+}
+function renderPage()  {
+  console.log('rendering page');
 }
 
-//see final page, return html for the final page
-function finalPage(){}
-
-//see the question area, return html for the question page
-function questionPage(){}
-
-//figure out which page to be on, returns html for that page
-function getPage(){}
-
-//display the correct page
-function renderPage(){
-  getPage();
+function getMainPage() {
+  console.log('getting main page');
 }
 
-//start a quiz
-function startQuiz(){}
+function handleStartPage() {
+  console.log('firstPage');
 
-//submit an answer
-function handleAnswerSubmit(){}
-
-//get feedback on their answer
-function handleAnswerResponse(){}
-
-//see their score
-function score(){
-  return STORE.score;
+  //listen to startQuiz button
+  
+  //renderStart page
+  
 }
 
-//see current question number 
-function questionNumber(){
-  return STORE.currentQuestion;
+function renderStartPage() {
+  console.log('start page rendering');
+  //render page html for start page
 }
+
+function handleQuizPage() {
+  console.log('handling quiz page');
+  //call getUserResponse
+    //call alertEmptyResponse
+}
+
+function getUserResponse() {
+  console.log('getting UserResponse');
+  //call scoreResponse
+  //call responseFeedback
+}
+
+function scoreResponse() {
+  console.log('scoring response');
+  //call compareResponseToAnswer
+  //update STORE score
+  //updateTicker()
+  //return gradingObj{}
+}
+
+function updateStoreScore() {
+  console.log('updating score in STORE');
+  //add  1 for gradingObj.correct ===true
+}
+
+function handleButtonChange(){
+  console.log('handleButtonChange works');
+}
+//changes the button when a user submits a response
+
+function toggleGrading(){
+  console.log('toggleGrading works');
+}
+//changes styling of the feedback if correct or incorrect
+ 
+function responseFeedback(){
+  console.log('responseFeedback works');
+}
+//returns the feedback for the answer submitted
+
+
+function alertEmptyRespose(){
+  console.log('alertEmptyResponse works');
+}
+//alerts the user when they dont sumbit an answer
+
+function handleEndPage(){
+  console.log('handleEndPage works');
+}
+//returns the html for the end page, shows end content, score, and restart quiz button
+
+function gradeResponse(){
+  console.log('gradeResponse works');
+}
+//returns the response for a question being correct or incorrect
+
+
+function updateTicker(){
+  console.log('updateTicker works');
+}
+
+function renderEndPage(){
+  console.log('renderEndPage works');
+}
+
+
+/*function restartQuiz(){
+  console.log('restartQuiz works');
+}*/
+//restarts the quiz, might not be needed
+
+function getScore(){
+  console.log('getScore works');
+}
+//returns STORE.score
 
 function main(){
-  /*firstPage();
-  renderPage();
-  startQuiz();
-  handleAnswerSubmit();
-  handleAnswerResponse();
-  score();
-  questionNumber();
-  finalPage();
-  renderQuestionArea();*/
-  handleFirstPage();
-  handleQuizPage();
-  handleFinalPage();
-  renderPage();
-}
 
-main() {
-    renderPage();
-    handleAnswerSubmit();
-    handleQuizStart();
-    handleNextQuestion();
 }
 
 $(main);
